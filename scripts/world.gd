8822a5f3f66c5e109b689cf167f27a40241492c7
Nils Harbke <nils.harbke@proton.me>
2024-04-06 20:33:31 +0200

    initialize target position

--- scripts/world.gd
+++ scripts/world.gd
@@ -14,4 +14,5 @@ func _process(_delta):
 	if Input.is_action_just_pressed("LeftClick"):
 		var settler: Settler = settler_scene.instantiate()
 		settler.global_position = Vector2(grid_map.hovered_tile)
+		settler.global_target_position = Vector2(grid_map.hovered_tile)
 		settlers.add_child(settler)
